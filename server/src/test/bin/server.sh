f7f4df9adb7cbcf46f718e9e41570c518c2c7ebe
otrack <0track@gmail.com>
2017-11-07 13:17:26 +0100

    [fix] slf4j; server.sh

--- server/src/test/bin/server.sh
+++ server/src/test/bin/server.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-IP=`/sbin/ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}'`
-PIP=`curl -s checkip.amazonaws.com`
 CLASSPATH="${DIR}/*:${DIR}/lib/*"
+IP="127.0.0.1"
+EXTRA="-rf 1"
 
-java -XX:+UseConcMarkSweepGC -Xms64m -Xmx2048m -Djava.net.preferIPv4Stack=true -Djgroups.tcp.address=${IP} -Dlog4j.configuration=log4j2.xml -cp ${CLASSPATH} org.infinispan.creson.Server -server ${IP}:11222 -proxy ${PIP}:11222 -rf 1
+if [ "$1" == "-ec2" ];
+then
+    echo "AWS EC2 mode"
+    IP=`/sbin/ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}'`
+    PIP=`curl -s checkip.amazonaws.com`
+    EXTRA="-proxy ${PIP}:11222 -ec2 -rf 2"
+fi
+
+java -XX:+UseConcMarkSweepGC -Xms64m -Xmx2048m -Djava.net.preferIPv4Stack=true -Djgroups.tcp.address=${IP} -Dlog4j.configuration=log4j2.xml -cp ${CLASSPATH} org.infinispan.creson.Server -server ${IP}:11222 ${EXTRA}
