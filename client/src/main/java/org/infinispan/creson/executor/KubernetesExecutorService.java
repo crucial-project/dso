72b8665733baeacb33c5b60bf197f483d36d68f9
otrack <0track@gmail.com>
2019-03-15 09:10:15 +0100

    initial k8s support

---
+++ client/src/main/java/org/infinispan/creson/executor/KubernetesExecutorService.java
@@ -0,0 +1,101 @@
+package org.infinispan.creson.executor;
+
+import io.kubernetes.client.ApiClient;
+import io.kubernetes.client.ApiException;
+import io.kubernetes.client.Configuration;
+import io.kubernetes.client.apis.BatchV1Api;
+import io.kubernetes.client.models.V1Job;
+import io.kubernetes.client.models.V1JobBuilder;
+import io.kubernetes.client.util.Config;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class KubernetesExecutorService implements ExecutorService {
+
+    private int parallelism;
+
+    public KubernetesExecutorService(int parallelism) throws IOException {
+        this.parallelism = parallelism;
+        ApiClient client = Config.defaultClient();
+        Configuration.setDefaultApiClient(client);
+    }
+
+    @Override
+    public void shutdown() {}
+
+    @Override
+    public List<Runnable> shutdownNow() {
+        return null;
+    }
+
+    @Override
+    public boolean isShutdown() {
+        return false;
+    }
+
+    @Override
+    public boolean isTerminated() {
+        return false;
+    }
+
+    @Override
+    public boolean awaitTermination(long timeout, TimeUnit unit) throws InterruptedException {
+        return false;
+    }
+
+    @Override
+    public <T> Future<T> submit(Callable<T> task) {
+        V1JobBuilder jobBuilder = new V1JobBuilder();
+        V1Job j = jobBuilder.build();
+        BatchV1Api api1 = new BatchV1Api();
+        try {
+            V1Job job = api1.createNamespacedJob("default",j,false,"true","false");
+        } catch (ApiException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public <T> Future<T> submit(Runnable task, T result) {
+        return null;
+    }
+
+    @Override
+    public Future<?> submit(Runnable task) {
+        return null;
+    }
+
+    @Override
+    public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks) throws InterruptedException {
+        return null;
+    }
+
+    @Override
+    public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException {
+        return null;
+    }
+
+    @Override
+    public <T> T invokeAny(Collection<? extends Callable<T>> tasks) throws InterruptedException, ExecutionException {
+        return null;
+    }
+
+    @Override
+    public <T> T invokeAny(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+        return null;
+    }
+
+    @Override
+    public void execute(Runnable command) {
+
+    }
+}
