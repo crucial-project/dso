61c591375da28fa7a1808ad5bbaa7b9666e9c69d
otrack <0track@gmail.com>
2019-04-05 20:02:32 +0200

    more fixes

---
+++ client/src/test/bin/k8s/exp-barriers.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+DIR=$(dirname "${BASH_SOURCE[0]}")
+source ${DIR}/utils_benchmarks.sh
+
+trap "pkill -KILL -P $$; exit 255" SIGINT SIGTERM
+
+# 1 - set-up
+k8s_rs_create ${TMPLDIR}/replicaset.yaml.tmpl 3 6 "LAUNCHED"
+k8s_rs_cp ${TMPLDIR}/replicaset.yaml.tmpl ${DIR}/../../../../target/infinispan-creson-client-9.4.1.Final.jar/ /tmp
+
+# 2 - barriers
+CALLS=1000
+PARALLELISM=(2 50 100 200 400)
+THREADS=1
+OUT=${LOGDIR}/barriers.dat
+OBJECT=("barrier" "sbarrier")
+echo -e "\t\tN\tF" > ${OUT}
+for p in ${PARALLELISM[@]};
+do
+    line=${p}"\t" 
+    for b in ${OBJECT[@]};
+    do
+	template=${TMPLDIR}/${b}-test.yaml.tmpl
+	start_access ${template} ${p} ${CALLS} ${THREADS}
+	wait_access ${template}
+	result=$(cat ${LOGDIR}/.log-* | grep -i Average | awk -F':' '{print $2}' | awk -F'[' '{print $1}' | awk '{ i++; s += $1 } END { print s/i }')
+	line=${line}"\t"${result}
+    done
+    echo -e ${line} >> ${OUT}
+done
+
+# 3 - Clean-up
+k8s_rs_delete ${TMPLDIR}/replicaset.yaml.tmpl
---
+++ client/src/test/bin/k8s/exp-counters.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+DIR=$(dirname "${BASH_SOURCE[0]}")
+source ${DIR}/utils_benchmarks.sh
+
+trap "pkill -KILL -P $$; exit 255" SIGINT SIGTERM
+
+template=${TMPLDIR}/counter-test.yaml.tmpl
+THREADS=1
+CALLS=10000
+REPLICAS=(3 6 9 12)
+OUT=${LOGDIR}/counters.dat
+echo -n "" > ${OUT}
+
+for r in ${REPLICAS[@]};
+do
+    line=${r}
+
+    k8s_rs_create ${TMPLDIR}/replicaset.yaml.tmpl ${r} 2 "LAUNCHED"
+    k8s_rs_cp ${TMPLDIR}/replicaset.yaml.tmpl ${DIR}/../../../../target/infinispan-creson-client-9.4.1.Final.jar/ /tmp
+
+    prev=0; cur=1; diff=1; p=32    
+    while [ ${diff} -gt 0 ] && [ ${p} -le 400 ];
+    do
+    	start_access ${template} ${p} ${CALLS} ${THREADS}
+    	wait_access ${template}
+
+	prev=${cur}
+    	cur=$(cat ${LOGDIR}/.log-* | grep -i Average | awk -F':' '{print $2}' | awk -F'=' '{print $2}' | sed s,\],,g | awk '{ s += $1 } END { print int(s) }')
+	diff=$(echo $(($((cur-prev))/1000))) # stop condition 
+	info ${r}" "${p}" "${cur}" ("${diff}")"
+
+    	p=$(echo "(3*${p})/2" | bc)
+    done
+    
+    k8s_rs_delete ${TMPLDIR}/replicaset.yaml.tmpl
+    
+    line=${line}"\t"${prev}
+    echo -e ${line} >> ${OUT}
+done
