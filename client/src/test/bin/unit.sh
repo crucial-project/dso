c31282dfd3b7859871085a56a4ec52757caaab2b
otrack <0track@gmail.com>
2020-11-13 12:58:19 +0100

    update

--- client/src/test/bin/unit.sh
+++ client/src/test/bin/unit.sh
@@ -24,19 +24,19 @@ fail(){
 
 start
 
-echo "Java tests"
-${DIR}/local/test.sh -counters
-${DIR}/local/test.sh -blobs
-${DIR}/local/test.sh -barrier
-${DIR}/local/test.sh -sbarrier
-${DIR}/local/test.sh -countdownlatch
+#echo "Java tests"
+#${DIR}/local/test.sh -counters
+#${DIR}/local/test.sh -blobs
+#${DIR}/local/test.sh -barrier
+#${DIR}/local/test.sh -sbarrier
+#${DIR}/local/test.sh -countdownlatch
 
 echo "Shell tests"
 source ${DIR}/aliases.sh
 THREADS=4
 [[ ! -z $(counter -n test reset; counter -n test increment -1 1 | grep "1") ]] || fail
 [[ ! -z $(counter -n test increment -1 1 | grep "2" ) ]] || fail
-[[ ! -z $(for i in $(seq 1 1 ${THREADS}); do counter -n test increment -1 1 & done | tail -n 1 | grep 6) ]] || fail
+[[ ! -z $(for i in $(seq 1 1 ${THREADS}); do counter -n test increment -1 1 & done | tail -n 1 | grep $((THREADS+2))) ]] || fail
 [[ ! -z $(for i in $(seq 1 1 ${THREADS}); do barrier -n test -p ${THREADS} await & done | grep ${THREADS}) ]] || fail
 
 success
