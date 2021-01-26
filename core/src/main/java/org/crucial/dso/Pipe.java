cf4161a873e720e9f3202c7adeb6a546ca52d714
Aurele MAHEO <aurele.maheo@gmail.com>
2021-01-26 16:38:36 +0100

    add method wait() in Pipe class => implement a barrier

--- core/src/main/java/org/crucial/dso/Pipe.java
+++ core/src/main/java/org/crucial/dso/Pipe.java
@@ -12,15 +12,35 @@ public class Pipe {
     @Option(names = "-n" )
     public String name = "pipe";
 
+    private static final int BARRIER = 2;
+    private AtomicCounter counter;
+    
+    public void wait()
+    {
+       int ret = counter.increment();
+
+       while(ret < BARRIER) {
+         try {
+           Thread.currentThread().sleep(500);
+         } catch (InterruptedException e) {
+          // ignore
+         }
+       }
+    }
+
     @Override
     @Command(name = "begin")
-    public void begin() {
-        
+    public void begin(@Option(names = "-1") String ipport) {
+
+      this.wait(); 
+    
     }
    
     @Override
     @Command(name = "end")
     public void end(@Option(names = "-1") String ipport) {
-        
+      
+      this.wait();
+
     }
 }
